07358336470ba3b8450f5696d7fd164550f1ce46
jjsebasv <jj.sebas.v@gmail.com>
2017-11-27 13:22:08 -0300

    Initial commit

---
+++ src/dospuntcero/Main.java
@@ -0,0 +1,39 @@
+package dospuntcero;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		while (true) {
+			BufferedReader st =new BufferedReader(new InputStreamReader (System.in));
+			
+			String s=st.readLine();	
+			
+			if (s.toLowerCase().equals("exit")) {
+				System.exit(0);
+			}
+
+			System.out.println(s);
+			
+			switch (s.toLowerCase()) {
+				case "exit": 
+					System.exit(0);
+					break;
+				case "gonzalo":
+					System.out.println("Cornejo");
+					break;
+				default:
+					System.out.println(s);
+					break;
+			
+			}
+			
+		}
+	}
+
+}
